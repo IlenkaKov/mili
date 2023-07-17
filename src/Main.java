386d01406fa8c67d1bddb3a74c6a31937fcde287
Elenka <ilenka2687@gmail.com>
2023-07-17 09:19:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 800; // количество потраченных денег на билет
+
+        int amount1 = 20; // количество потраченных рублей за которые начисляется 1 миля
+
+        System.out.println((amount
+                / amount1) + "начисленно миль");
+    }
+}
